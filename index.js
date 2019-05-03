5be23333c010a621de7de08ce056a61a4b920d5c
emschenn <emilynov14@gmail.com>
2019-05-03 20:14:34 +0800

    add slide

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
-    $("#button").toggle()
-    $("two").toggle();
-    $("#topic").click(function(){
-        $("one").toggle("slow");
-        $("two").toggle("slow");  
-        $("#button").toggle("slow")
-    });
+    var mySwiper = new Swiper(".swiper-container", {
+        // Optional parameters
+        direction: "horizontal",
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      });
+    // $("#button").toggle()
+    // $("two").toggle();
+    // $("#topic").click(function(){
+    //     $("one").toggle("slow");
+    //     $("two").toggle("slow");  
+    //     $("#button").toggle("slow")
+    // });
     $("#button").click(function(){
 
     });
